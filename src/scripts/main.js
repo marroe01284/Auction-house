6c501e3571a0da43f4e6dc882e9429240f4a9e25
Marius Rønning <mpr941@hotmail.com>
2024-12-03 15:17:57 +0100

    Create the landing page FEAT: added content to the cards and added a new button layout for a better experience.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
-console.log("test");
+    // Scroll effect
+    const scrollElements = document.querySelectorAll(".scroll-animation");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("scroll-visible");
+          } else {
+            entry.target.classList.remove("scroll-visible");
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    scrollElements.forEach((el) => observer.observe(el));
